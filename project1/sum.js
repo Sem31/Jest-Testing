a977d27be3559ec8629d23d3bca52738f00596fe
semprajapat <31kamleshprajapat@gmail.com>
2020-12-08 23:25:32 -0700

    first project1

--- project1/sum.js
+++ project1/sum.js
@@ -1,4 +1,17 @@
 function sum(a, b) {
     return a + b;
   }
+
+// function arrayList(){
+//     const shoppingList = [
+//         'diapers',
+//         'kleenex',
+//         'trash bags',
+//         'paper towels',
+//         'milk',
+//     ];
+//     return shoppingList
+// }
+
 module.exports = sum;
+// module.exports = arrayList;
--- project1/sum.test.js
+++ project1/sum.test.js
@@ -17,3 +17,54 @@ test('adding positive numbers is not zero. Negative assertions case to check', (
         }
     }
 });
+
+test('null value test assertions', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+  });
+  
+test('zero value test assertions', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
+
+test('two plus two addition assertions', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+  
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+  });
+
+test('there is no I in team', () => {
+  expect('team').not.toMatch(/I/);
+});
+  
+test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+});
+
+const arrayList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'milk',
+  ];
+  
+test('to check list contain milk or not ', () => {
+    expect(arrayList).toContain('milk');
+    expect(new Set(arrayList)).toContain('milk');
+});
