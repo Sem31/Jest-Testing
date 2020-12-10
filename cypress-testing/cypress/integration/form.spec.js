30bd1e2053cb93f5baaf3161afdff97a5111dc76
semprajapat <31kamleshprajapat@gmail.com>
2020-12-09 19:05:50 -0700

    cypress project code

---
+++ cypress-testing/cypress/integration/form.spec.js
@@ -0,0 +1,36 @@
+describe("Form test", () => {
+    //
+    it("Can fill the form", () => {
+        //
+        cy.visit("/");
+        cy.get("form");
+
+        // cy.get('input[name="name"]').type("Molly");
+
+        //code for test functionality
+        cy.get('input[name="name"]')
+            .type("Molly")
+            .should("have.value", "Molly");
+
+        cy.get('input[name="email"]')
+            .type("molly@dev.dev")
+            .should("have.value", "molly@dev.dev");
+        
+        cy.get("textarea")
+            .type("Mind you if I ask some silly question?")
+            .should("have.value", "Mind you if I ask some silly question?");
+        
+        // cy.get("form").submit();
+        // XHR request handling
+        cy.server();
+        cy.route({
+        url: "/users/**",
+        method: "POST",
+        response: { status: "Saved", code: 201 }
+        });
+
+        cy.get("form").submit();
+
+        cy.contains("Saved");
+      });
+  });
